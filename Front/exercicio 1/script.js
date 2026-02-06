10f26c81dfb8372e946e10adb75a9b698d2d8923
Gabriel Bernardi <115042820+GoldenLeap@users.noreply.github.com>
2026-02-06 16:51:00 -0300

    Finaliza exercicio 1 e começa exercicio 2

---
+++ Front/exercicio 1/script.js
@@ -0,0 +1,14 @@
+const celsiusElement = document.getElementById("celsius");
+
+function celsiusToFahrenheit (){
+    let celsius = parseFloat(celsiusElement.value);
+    if(isNaN(celsius)) return alert("Invalido");
+    fahrenheit = celsius * 1.8 + 32;
+        if(fahrenheit >= 80){
+            document.body.style.backgroundColor = "coral";
+        }else{
+            document.body.style.backgroundColor = "lightskyblue"
+        }
+    
+
+}
