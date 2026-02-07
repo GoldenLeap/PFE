54b44935f9bc0f02631216a4da76e47a166e0638
Gabriel Bernardi <115042820+GoldenLeap@users.noreply.github.com>
2026-02-06 23:28:29 -0300

    Finaliza exercicio 2

--- Front/exercicio 2/script.js
+++ Front/exercicio 2/script.js
@@ -2,11 +2,18 @@ const cardName =  document.getElementById("nome");
 const nameInput =  document.getElementById("nomeInput");
 const cardCargo =  document.getElementById("cargo");
 const cargoInput =  document.getElementById("cargoInput");
+const cardColor = document.getElementById("cardCol")
+const card = document.querySelector(".card");
+
 function updateName(){
-    cardName.innerHTML = nameInput.value;
+    cardName.textContent = nameInput.value || '[digite o nome]';
 
 }
 function updateCargo(){
-    cardCargo.innerHTML = cargoInput.value;
+    cardCargo.textContent = cargoInput.value;
+}
+function updateCardCol(){
+    let color = cardColor.value || '-';
+    card.style.setProperty("--card-color", color)
 
 }
